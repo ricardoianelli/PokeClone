ceda51f4c9f978bf7da78efa722b4501c36e86eb
Ricardo Ianelli <ricardo.ianelli@stone.com.br>
2020-01-13 00:20:45 -0300

    Refactor Keyboard movement input

--- Assets/Scripts/Util/KeyboardInput.cs
+++ Assets/Scripts/Util/KeyboardInput.cs
@@ -1,10 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace Util
 {
     public class KeyboardInput : MovementInput
     {
         private Direction _lastDirection = Direction.None;
+        private IList<KeyCode> _keysPressed = new List<KeyCode>();
+        
+        private readonly  IList<KeyCode> _keyCodeList = new List<KeyCode>()
+            {KeyCode.UpArrow, KeyCode.DownArrow, KeyCode.RightArrow, KeyCode.LeftArrow};
 
         public override Direction GetMovementDirection()
         {
@@ -13,32 +19,41 @@ namespace Util
 
         private void Update()
         {
-            _lastDirection = GetDirectionByKey();
+            UpdatePressedKeys();
+            _lastDirection = GetDirectionByKeycode(_keysPressed.LastOrDefault());
         }
-        
-        private static Direction GetDirectionByKey()
-        {
-            if (Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                return Direction.North;
-            }
-            
-            if (Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                return Direction.East;
-            }
-            
-            if (Input.GetKeyDown(KeyCode.DownArrow))
-            {
-                return Direction.South;
-            }
-            
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                return Direction.West;
-            }
 
-            return Direction.None;
+        private static Direction GetDirectionByKeycode(KeyCode keycode)
+        {
+            switch (keycode)
+            {
+                case KeyCode.UpArrow:
+                    return Direction.North;
+                case KeyCode.RightArrow:
+                    return Direction.East;
+                case KeyCode.DownArrow:
+                    return Direction.South;
+                case KeyCode.LeftArrow:
+                    return Direction.West;
+                default:
+                    return Direction.None;
+            }
+        }
+
+        private void UpdatePressedKeys()
+        {
+            foreach (var keyCode in _keyCodeList)
+            {
+                if (Input.GetKeyDown(keyCode))
+                {
+                    _keysPressed.Add(keyCode);
+                }
+
+                if (Input.GetKeyUp(keyCode))
+                {
+                    _keysPressed.Remove(keyCode);
+                }
+            }
         }
     }
 }
