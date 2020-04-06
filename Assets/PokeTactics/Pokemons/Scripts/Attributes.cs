3efc3e027d11d64ab6cddf264c9581db49c3df47
Ricardo Ianelli <ricardo.ianelli@stone.com.br>
2020-04-05 22:45:28 -0300

    Add attributes class

---
+++ Assets/PokeTactics/Pokemons/Scripts/Attributes.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace PokeTactics.Pokemons.Scripts
+{
+    public class Attributes
+    {
+        public Attribute Attack;
+        public Attribute SpecialAttack;
+        public Attribute Defense;
+        public Attribute SpecialDefense;
+        public Attribute Speed;
+
+        public Attributes()
+        {
+            Attack = new Attribute("Attack");
+            SpecialAttack = new Attribute("Sp. Attack");
+            Defense = new Attribute("Defense");
+            SpecialDefense = new Attribute("Sp. Defense");
+            Speed = new Attribute("Speed");  
+        }
+    }
+
+    public class Attribute
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public int Modifier { get; set; }
+
+        public Attribute()
+        {
+            Value = 1;
+            Modifier = 0;
+        }
+
+        public Attribute(string name) : this()
+        {
+            Name = name;
+        }
+    }
+}
